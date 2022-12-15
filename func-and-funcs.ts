e94a8aebe2c13d79030d4089f663ad6f4f99e843
Juno Kim <lippoint.surf0622@gmail.com>
2022-12-15 20:43:54 +0900

    func and funcs

---
+++ func-and-funcs.ts
@@ -0,0 +1,13 @@
+export function printToFile(text: string, callback: () => void): void {
+  console.log(text);
+  callback();
+}
+
+export function arrayMutate(
+  numbers: number[],
+  mutate: (v: number) => number
+): number[] {
+  return numbers.map(mutate);
+}
+
+console.log(arrayMutate([1, 20, 3], (v) => v * 10));
