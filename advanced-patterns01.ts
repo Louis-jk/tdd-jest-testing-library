711f2d35bf144dd7a40de01449da223fe0eab287
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-04 22:21:17 +0900

    Observer 추가

--- advanced-patterns01.ts
+++ advanced-patterns01.ts
@@ -1,3 +1,23 @@
+// Observer
+type Listener<EventType> = (ev: EventType) => void;
+function createObserver<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  let listeners: Listener<EventType>[] = [];
+  return {
+    subscribe: (listener: Listener<EventType>): (() => void) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter((l) => l !== listener);
+      };
+    },
+    publish: (event: EventType) => {
+      listeners.forEach((l) => l(event));
+    },
+  };
+}
+
 interface Pocketmon {
   id: string;
   attack: number;
