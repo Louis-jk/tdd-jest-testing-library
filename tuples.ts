de332a1d72b418d9df74bb7a057a6d26ba32470a
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-05 22:28:48 +0900

    array getter, setter 생성 react의 useState와 같은 맥락

--- tuples.ts
+++ tuples.ts
@@ -7,4 +7,21 @@ function add3DCoordinate(
   return [c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]];
 }
 
-console.log(add3DCoordinate([0, 0, 0], [10, 20, 30]));
+console.log(add3DCoordinate([0, 100, 0], [10, 20, 30]));
+
+function simpleStringState(
+  initial: string
+): [() => string, (v: string) => void] {
+  let str: string = initial;
+  return [
+    () => str,
+    (v: string) => {
+      str = v;
+    },
+  ];
+}
+
+const [str1getter, str1setter] = simpleStringState('hello');
+console.log(str1getter());
+str1setter('Joonho');
+console.log(str1getter());
