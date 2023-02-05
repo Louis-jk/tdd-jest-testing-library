875f3f0860c01c178b111840825997f1e72805c4
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-05 22:31:03 +0900

    setter, getter testing

--- tuples.ts
+++ tuples.ts
@@ -22,6 +22,9 @@ function simpleStringState(
 }
 
 const [str1getter, str1setter] = simpleStringState('hello');
+const [str2getter, str2setter] = simpleStringState('Joonho');
+console.log(str2getter());
 console.log(str1getter());
-str1setter('Joonho');
+str1setter('goodbye');
 console.log(str1getter());
+console.log(str2getter());
