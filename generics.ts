fdd02ab32d5fa88d49b7b57cafd25e88cde57be7
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-06 16:23:21 +0900

    function generic

--- generics.ts
+++ generics.ts
@@ -17,3 +17,22 @@ const [st2getter, st2setter] = simpleState<number | null>(null);
 console.log(st2getter());
 st2setter(10);
 console.log(st2getter());
+
+interface Rank<RankItem> {
+  item: RankItem;
+  rank: number;
+}
+
+function ranker<RankItem>(
+  items: RankItem[],
+  rank: (v: RankItem) => number
+): RankItem[] {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rank(item),
+  }));
+
+  ranks.sort((a, b) => a.rank - b.rank);
+
+  return ranks.map((rank) => rank.item);
+}
