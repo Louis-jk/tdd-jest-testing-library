e9237d07be910bf27096d01c0613262fa7bd5570
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-06 12:22:17 +0900

    generic change

--- generics.ts
+++ generics.ts
@@ -1,13 +1,11 @@
-const a1: Array<string> = ['foo', 'bar', 'baz'];
-const a2: Array<number> = [1, 2, 3];
-
-const x = a1.pop();
-const y = a2.pop();
-
-const p1 = Promise.resolve('hello!');
-const p2 = Promise.resolve(3.14);
-
-(async () => {
-  const x = await p1;
-  const y = await p2;
-})();
+function simpleStringState(
+  initial: string
+): [() => string, (v: string) => void] {
+  let str: string = initial;
+  return [
+    () => str,
+    (v: string) => {
+      str = v;
+    },
+  ];
+}
