58fd72b05f5cec9fa19ac94d816c776257791d13
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-09 16:34:39 +0900

    enum 추가

---
+++ enums.ts
@@ -0,0 +1,14 @@
+// const beforeLoad = 'beforeLoad';
+// const loading = 'loading';
+// const loaded = 'loaded';
+
+enum LoadingState {
+  beforeLoad = 'beforeLoad',
+  loading = 'loading',
+  loaded = 'loaded',
+}
+
+const isLoading = (state: LoadingState) => state === LoadingState.loading;
+
+console.log(isLoading(LoadingState.beforeLoad));
+console.log(isLoading(LoadingState.loading));
