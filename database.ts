77565f303c7f3f2eff3a36d0d40e5beba74198bd
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-09 20:38:20 +0900

    memory 별도 저장 객체 표현

--- database.ts
+++ database.ts
@@ -3,8 +3,12 @@ interface Database {
   set(id: string, value: string): void;
 }
 
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storeState: string): void;
+}
 class InMemoryDatabase implements Database {
-  db: Record<string, string> = {};
+  protected db: Record<string, string> = {};
   get(id: string): string {
     return this.db[id];
   }
@@ -13,7 +17,28 @@ class InMemoryDatabase implements Database {
   }
 }
 
-const myDB = new InMemoryDatabase();
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+  restoreFromString(storeState: string): void {
+    this.db = JSON.parse(storeState);
+  }
+}
+
+const myDB = new PersistentMemoryDB();
 myDB.set('foo', 'bar');
-myDB.db['foo'] = 'baz';
+// myDB.db["foo"] = "baz"
+
+console.log(myDB.get('foo'));
+// console.log(myDB.saveToString());
+// console.log(myDB.restoreFromString('baz'));
+
+const saved = myDB.saveToString();
+myDB.set('foo', 'db1 - bar');
+console.log(myDB.get('foo'));
+
+const myDB2 = new PersistentMemoryDB();
+myDB2.restoreFromString(saved);
+console.log(myDB2.get('foo'));
 console.log(myDB.get('foo'));
