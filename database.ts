99ed27ae6078369683c46eb5c8cf32f0a0d07033
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-10 19:36:05 +0900

    Keyof 추가

--- database.ts
+++ database.ts
@@ -1,23 +1,28 @@
-interface Database<T> {
-  get(id: string): T;
-  set(id: string, value: T): void;
+interface Database<T, K> {
+  get(id: K): T;
+  set(id: K, value: T): void;
 }
 
 interface Persistable {
   saveToString(): string;
   restoreFromString(storeState: string): void;
 }
-class InMemoryDatabase<T> implements Database<T> {
-  protected db: Record<string, T> = {};
-  get(id: string): T {
+
+type DBKeyType = string | number | symbol;
+class InMemoryDatabase<T, K extends DBKeyType> implements Database<T, K> {
+  protected db: Record<K, T> = {} as Record<K, T>;
+  get(id: K): T {
     return this.db[id];
   }
-  set(id: string, value: T): void {
+  set(id: K, value: T): void {
     this.db[id] = value;
   }
 }
 
-class PersistentMemoryDB<T> extends InMemoryDatabase<T> implements Persistable {
+class PersistentMemoryDB<T, K extends DBKeyType>
+  extends InMemoryDatabase<T, K>
+  implements Persistable
+{
   saveToString(): string {
     return JSON.stringify(this.db);
   }
@@ -26,7 +31,7 @@ class PersistentMemoryDB<T> extends InMemoryDatabase<T> implements Persistable {
   }
 }
 
-const myDB = new PersistentMemoryDB<string>();
+const myDB = new PersistentMemoryDB<string, string>();
 myDB.set('foo', 'bar');
 // myDB.db["foo"] = "baz"
 
@@ -38,7 +43,7 @@ const saved = myDB.saveToString();
 myDB.set('foo', 'db1 - bar');
 console.log(myDB.get('foo'));
 
-const myDB2 = new PersistentMemoryDB<string>();
+const myDB2 = new PersistentMemoryDB<string, string>();
 myDB2.restoreFromString(saved);
 console.log(myDB2.get('foo'));
 console.log(myDB.get('foo'));
