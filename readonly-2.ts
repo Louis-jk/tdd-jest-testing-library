1462dead5c67e00b5a08dc0e741ec36c7e231129
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-12 18:01:59 +0900

    클래스 내부 누구나 알수 있는 정보를 담아서 호출하고 싶을 때 public readonly

--- readonly-2.ts
+++ readonly-2.ts
@@ -1,7 +1,9 @@
 class Doggy {
+  public readonly foo: number = 2;
   constructor(public readonly name: string, public age: number) {}
 }
 
 const lgg = new Doggy('LG', 13);
-// lgg.name = 'Foo';
+
 console.log(lgg.name);
+console.log(lgg.foo);
