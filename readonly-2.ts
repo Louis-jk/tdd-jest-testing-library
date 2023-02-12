7775ebfa1e7701ad64464ee316c1e6087b7647e4
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-12 18:05:31 +0900

    javascript 싱글톤 구현

--- readonly-2.ts
+++ readonly-2.ts
@@ -1,9 +1,19 @@
 class Doggy {
-  public readonly foo: number = 2;
   constructor(public readonly name: string, public age: number) {}
 }
 
 const lgg = new Doggy('LG', 13);
 
 console.log(lgg.name);
-console.log(lgg.foo);
+
+class DogList {
+  private doggies: Doggy[] = [];
+
+  static instance: DogList = new DogList();
+
+  private constructor() {} // javascript 싱글톤 구현
+}
+
+DogList.instance; // javascript 싱글톤 구현
+
+// const dl = new DogList(); javascript 싱글톤 구현
