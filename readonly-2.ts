8b4de78a91c9883b1fd0a11c353071b997bfb729
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-12 18:13:20 +0900

    static을 사용한 것과 클래스 내부에서 instance를 사용하여 싱글톤으로 처리

--- readonly-2.ts
+++ readonly-2.ts
@@ -3,6 +3,7 @@ class Doggy {
 }
 
 const lgg = new Doggy('LG', 13);
+lgg.age = 22;
 
 console.log(lgg.name);
 
@@ -13,11 +14,16 @@ class DogList {
 
   private constructor() {} // javascript 싱글톤 구현
 
-  public addDog(dog: Doggy) {
-    this.doggies.push(dog);
+  static addDog(dog: Doggy) {
+    DogList.instance.doggies.push(dog);
+  }
+
+  getDogs() {
+    return this.doggies;
   }
 }
 
-DogList.instance.addDog(lgg); // javascript 싱글톤 구현
+DogList.addDog(lgg);
+console.log(DogList.instance.getDogs());
 
 // const dl = new DogList(); javascript 싱글톤 구현
