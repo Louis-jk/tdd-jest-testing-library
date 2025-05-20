ca5dc8822b2ed41eb238cd299740fdc7919863f7
Joonho Kim <lippoint.surf0622@gmail.com>
2025-05-20 15:41:37 +0900

    run and test jest test options

--- src/components/great/Great.test.tsx
+++ src/components/great/Great.test.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Great from "./Great";
 
-test("挨拶が正常にレンダリングされる", () => {
+test("render without a name prop", () => {
     render(<Great />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
 });
 
 
-test("挨拶と名前を一緒にレンダリングされる", () => {
+test("render with a name prop", () => {
     render(<Great name="ShinCode" />);
     const textElement = screen.getByText("Hello ShinCode");
     expect(textElement).toBeInTheDocument();
