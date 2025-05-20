8cc8dd249774ff3f42b58440d68da367d6ef6131
Joonho Kim <lippoint.surf0622@gmail.com>
2025-05-20 14:45:08 +0900

    test

--- functions-test.ts
+++
@@ -1,6 +0,0 @@
-import { addNumbers, printFormat, fetchData, getName } from './functions';
-
-console.log(addNumbers(1, 2));
-console.log(printFormat('joonho', 100));
-console.log(fetchData('https://joonhokim.dev'));
-console.log(getName({ first: 'joonho', last: 'Kim' }));
