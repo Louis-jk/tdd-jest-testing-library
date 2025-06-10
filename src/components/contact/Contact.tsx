719ae894a628745f7bda81e77c9be5b5a4a944c2
Joonho Kim <lippoint.surf0622@gmail.com>
2025-06-10 12:02:21 +0900

    created Contact component and modify App.tsx

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -10,7 +10,26 @@ const Contact = () => {
             <div>
                 <label htmlFor="name">名前</label>
                 <input type="text" id="name" name="name" required />
-            </div>            
+            </div>
+
+            <div>
+                <label htmlFor="question">お問い合わせ内容</label>
+                <select id="question" name="question" required>
+                    <option value="">選択してください</option>
+                    <option value="dev">開発案件のご相談</option>
+                    <option value="video">撮影のご相談</option>
+                    <option value="sns-marketing">SNSマーケティングのご相談</option>
+                </select>
+            </div>
+
+            <div>
+                <label>
+                <input type="checkbox" id="terms" />
+                利用規約に同意します。
+                </label>
+            </div>
+
+            <button type="submit">送信</button>
         </form>
     </div>
   )
