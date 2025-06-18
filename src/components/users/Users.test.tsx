5738cc5b3f2b335f6540311b85fc59b1dc1e4b86
Joonho Kim <lippoint.surf0622@gmail.com>
2025-06-18 15:22:45 +0900

    users test

---
+++ src/components/users/Users.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import Users from "./Users"
+
+describe("Users", () => {
+
+    test("renders correctly", () => {
+        render(<Users />)
+        const textElement = screen.getByRole("heading", {
+            name: "Users"
+        })
+        expect(textElement).toBeInTheDocument()
+    })
+
+    test("renders a list of users", async () => {
+        render(<Users />)        
+        const users = await screen.findAllByRole("listitem")
+        expect(users).toHaveLength(10)
+    })
+})
