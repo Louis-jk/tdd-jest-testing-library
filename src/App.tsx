4c2772297bce4a2e336f5781061972e94f24b524
Joonho Kim <lippoint.surf0622@gmail.com>
2025-06-18 15:18:59 +0900

    api의 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Counter from './components/counter/Counter';
 import Contact from './components/contact/Contact';
+import Users from './components/users/Users';
 
 function App() {
   return (
     <div className="App">
       <Contact />
       <Counter />
+      <Users />
     </div>
   );
 }
