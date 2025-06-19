3df8d20376d8fded4d9a047bd0592f9d57e84548
Joonho Kim <lippoint.surf0622@gmail.com>
2025-06-19 11:14:59 +0900

    msw test

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -2,22 +2,34 @@ import React, { useEffect, useState } from 'react'
 
 function Users() {
     const [users, setUsers] = useState<string[]>([])
+    const [error, setError] = useState<string | null>(null)
+
+    const fetchUsers = async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users")
+            const data = await response.json()
+            setUsers(data.map((user: { name: string }) => user.name))
+            setError(null)
+        } catch (error) {
+            setError("Error fetching users")
+        }
+    }
+
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(res => res.json())
-        .then(data => setUsers(data.map((user: { name: string }) => user.name)))
+        fetchUsers()
     }, [])
 
-  return (
-    <div>
-        <h1>Users</h1>
-        <ul>
-            {users.map((user) => (
-                <li key={user}>{user}</li>
-            ))}
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Users</h1>
+            {error && <p>{error}</p>}
+            <ul>
+                {users.map((user) => (
+                    <li key={user}>{user}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Users
